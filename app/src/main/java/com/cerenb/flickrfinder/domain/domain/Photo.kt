993cc3148ca2f835fb6a0969b9ccc0558a8e9d2e
ceren.buyukkardes <crnckr@gmail.com>
2023-03-02 00:12:15 +0100

    [REMOVE] Remove duplicated domain model

--- app/src/main/java/com/cerenb/flickrfinder/domain/domain/Photo.kt
+++
@@ -1,9 +0,0 @@
-package com.cerenb.flickrfinder.domain.domain
-
-data class Photo(
-    val id: String,
-    val title: String,
-    val owner: String,
-    val secret: String,
-    val server: String
-)
